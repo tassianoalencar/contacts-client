56dd516ddb821461419bed658cca7d155e5d6bf0
Tassiano Alencar <tassiano.alencar@icloud.com>
2021-02-23 21:29:42 -0300

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    // Some useful comment
+    fontFamily: {
+      'sans': ['nunito', 'sans-serif']
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
